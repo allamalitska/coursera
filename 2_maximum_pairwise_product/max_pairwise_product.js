82149864dfb6146d48c1a964a0d961e81161a9c3
allamalitskad <allamalitskad@gmail.com>
2021-10-29 17:14:06 +0300

    Test

--- 2_maximum_pairwise_product/max_pairwise_product.js
+++ 2_maximum_pairwise_product/max_pairwise_product.js
@@ -19,7 +19,33 @@ function readLine (line) {
 }
 
 function max(arr) {
-    // write your code here
+    let n = arr.length-1
+    let pairSums = []
+    let maxValue = 0;
+        for (i=0; i< arr.length-2; i++)
+            for (j=1; j< arr.length -1; j++)
+                totalPairs.push([arr[i],arr[j]])
+                pairSums.push([arr[i]*arr[j]])
+                maxValue = Math.max(maxValue, aar[i]*arr[j])
+
 }
+
+const value = maxPairSum([5,4,3,1])
+
+
+
+const superSimpleTests = (exampleArray, expectedValue)=>{
+
+    if(sumArrayPairs(exampleArray)===expectedValue){
+    console.log('This example passes!')
+    }
+    console.log('This example failse!')
+    }
+    
+    superSimpleTests([5,4,3,1], 20)
+    superSimpleTests([], 0)
+    superSimpleTests([5], 0)
+    superSimpleTests([], 0)
+    superSimpleTests([5,5], 25)
 
 module.exports = max;
