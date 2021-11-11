0ce40bc9d747e0881dc5b165b74c79b42500c580
allamalitskad <allamalitskad@gmail.com>
2021-11-11 14:01:54 +0200

    "fixed test passing AplusB"

--- 2_maximum_pairwise_product/max_pairwise_product.py
+++ 2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,15 +1,30 @@
+
 def max_pairwise_product(numbers):
-    n = len(numbers)
-    max_product = 0
-    for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
+# should be at least two items in the arr
+    if len(numbers) < 1:
+        return -1
+#set value for maxFirst
+    maxFirstIndex = 0
+#iterate the arr
+    for i in range(len(numbers)):
+#compearing items
+        if numbers[maxFirstIndex] < numbers[i]:
+            maxFirstIndex = i
+    print(numbers[maxFirstIndex])
 
-    return max_product
+#set value for maxSecond
+    maxSecondIndex = 0
+#iterate the arr
+    for i in range(len(numbers)):
+# maxSecond shouldn't equal maxFirst
+        if i != maxFirstIndex and numbers[maxSecondIndex] < numbers[i]:
+            maxSecondIndex = i
+    print (numbers[maxSecondIndex])
+        
+    return numbers[maxSecondIndex]* numbers[maxFirstIndex]
 
+    
+    
+result = max_pairwise_product([12, 8, 11,15])
 
-if __name__ == '__main__':
-    input_n = int(input())
-    input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+print(result)
